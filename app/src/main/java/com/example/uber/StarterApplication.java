bbb2cb48e74a58f9d420b222ca855328c12d42a3
BautiCardenau <bcardenau@itba.edu.ar>
2020-07-30 13:43:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/uber/StarterApplication.java
@@ -0,0 +1,37 @@
+package com.example.uber;
+
+
+import android.app.Application;
+import android.util.Log;
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("myappID")
+                .clientKey("BYJy1F9XKKqb")
+                .server("http://3.19.60.240/parse/")
+                .build()
+        );
+
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+}
